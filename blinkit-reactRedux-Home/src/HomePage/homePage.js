b52daec0b53208f019b366077b358869ad723d0d
abasotia <abasotia@tekion.com>
2023-03-14 15:36:45 +0530

    commited changes

--- blinkit-reactRedux-Home/src/HomePage/homePage.js
+++ blinkit-reactRedux-Home/src/HomePage/homePage.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import ReactDOM from "react-dom/client";
 import ProductData from "../assets/Data/ProductData.js"
 import NavBar from  "./organisms/NavBar/navBar";
 import NavMenu from "./organisms/NavMenu/navMenu";
@@ -8,6 +7,8 @@ import Footer from "./organisms/Footer/footer"
 import sortByName from "./Helper/sortByName.js"
 import sortByDiscountLowHigh from "./Helper/sortByDiscountLowHigh.js"
 import sortByDiscountHighLow from "./Helper/sortByDiscountHighLow.js"
+import { connect } from 'react-redux';
+import { addToCart, increaseProductToCart, decreaseProductCart } from '../redux/cartActionTypes.js';
 
 class HomePage extends Component {
 constructor(props)
@@ -15,37 +16,36 @@ constructor(props)
     super(props);
     this.state = {
         productsDisplayed: Object.values(ProductData).sort((x,y) => x.productName.localeCompare(y.productName)),
-        cart: {}
     }
 }
 
-handleAddToCart(productName)
-{
-    let tempCart = {...this.state.cart};
-    tempCart[productName] = 1;
-    this.setState(this.state.cart = tempCart);
-}
+// handleAddToCart(productName)
+// {
+//     let tempCart = {...this.state.cart};
+//     tempCart[productName] = 1;
+//     this.setState(this.state.cart = tempCart);
+// }
 
-handleIncreaseProductUnit(productName){
-    let tempCart = {...this.state.cart};
-    tempCart[productName] += 1;
-    this.setState(this.state.cart = tempCart);
-    console.log("Cart on inc", this.state.cart);
-}
+// handleIncreaseProductUnit(productName){
+//     let tempCart = {...this.state.cart};
+//     tempCart[productName] += 1;
+//     this.setState(this.state.cart = tempCart);
+//     console.log("Cart on inc", this.state.cart);
+// }
 
-handleDecreaseProductUnit(productName){
-    let tempCart = {...this.state.cart};
-    tempCart[productName] -= 1;
-    if(tempCart[productName] == 0)
-    {
-        // let { productName, ...removedProductName} = tempCart;
-        // tempCart = removedProductName
-        delete tempCart[productName]
-    }
-    this.setState(this.state.cart = tempCart);
-    console.log("cart on decrease", this.state.cart);
+// handleDecreaseProductUnit(productName){
+//     let tempCart = {...this.state.cart};
+//     tempCart[productName] -= 1;
+//     if(tempCart[productName] == 0)
+//     {
+//         // let { productName, ...removedProductName} = tempCart;
+//         // tempCart = removedProductName
+//         delete tempCart[productName]
+//     }
+//     this.setState(this.state.cart = tempCart);
+//     console.log("cart on decrease", this.state.cart);
 
-}
+// }
 
 handleKeyUp(searchValue)
 {
@@ -89,14 +89,14 @@ handleSortBy(sortByValue)
 render() {
     return (
       <>
-        <NavBar cart={this.state.cart}
+        <NavBar cart={this.props.cart}
                  handleKeyUp={this.handleKeyUp.bind(this)} /> 
         <NavMenu />
-        <MainBody cart={this.state.cart} 
+        <MainBody cart={this.props.cart} 
                   productsDisplayed={this.state.productsDisplayed}
-                  addToCartHandler={this.handleAddToCart.bind(this)} 
-                  increaseProductUnit={this.handleIncreaseProductUnit.bind(this)}
-                  decreaseProductUnit={this.handleDecreaseProductUnit.bind(this)}
+                  addToCartHandler={this.props.addToCart} 
+                  increaseProductUnit={this.props.increaseProductToCart}
+                  decreaseProductUnit={this.props.decreaseProductCart}
                   sortBy={this.handleSortBy.bind(this)}         
         />
         <Footer />  
@@ -105,4 +105,20 @@ render() {
   }
 }
 
-export default HomePage;
+const mapStateToProps = (state) => {
+    return {
+        cart : state.cart
+    }
+}
+
+const mapDispatchToProps = (dispatch) => {
+
+    return {
+        addToCart : (productName) => dispatch(addToCart(productName)),
+        increaseProductToCart : (productName) => dispatch(increaseProductToCart(productName)),
+        decreaseProductCart : (productName) => dispatch(decreaseProductCart(productName))
+    }
+
+}
+
+export default connect(mapStateToProps, mapDispatchToProps )(HomePage);
